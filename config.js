4aa4e5e109e57133f9aea919c7eef96f0584f3be
policeduafatima <policeduafatima@gmail.com>
2025-07-14 20:46:34 +0500

    Update config.js

--- config.js
+++ config.js
@@ -41,11 +41,11 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "923155641171",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "923306137477",
 // add your bot owner number
-OWNER_NAME: process.env.OWNER_NAME || "DUAFATIMA",
+OWNER_NAME: process.env.OWNER_NAME || "𝐸𝑅𝐹𝒜𝒩 𝒜𝐻𝑀𝒜𝒟, DUA-FATIMA",
 // add bot owner name
-DESCRIPTION: process.env.DESCRIPTION || "*© CREATER DUA FATIMA *",
+DESCRIPTION: process.env.DESCRIPTION || "*© CREATER 𝐸𝑅𝐹𝒜𝒩 𝒜𝐻𝑀𝒜𝒟 * , *DUA-FATIMA*",
 // add bot owner name    
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/bgwzkl.jpg",
 // add img for alive msg
@@ -73,7 +73,7 @@ AUTO_TYPING: process.env.AUTO_TYPING || "false",
 // true for automatic show typing   
 READ_CMD: process.env.READ_CMD || "false",
 // true if want mark commands as read 
-DEV: process.env.DEV || "923155641171",
+DEV: process.env.DEV || "923306137477",
 //replace with your whatsapp number        
 ANTI_VV: process.env.ANTI_VV || "true",
 // true for anti once view 
