80f1aeccf740e6cce33a795f6cc3f08cb7bffacf
policeduafatima <policeduafatima@gmail.com>
2025-07-19 11:33:25 +0500

    Update config.js

--- config.js
+++ config.js
@@ -31,9 +31,9 @@ MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/r2ncqh",
 // add custom menu and mention reply image url
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
-BOT_NAME: process.env.BOT_NAME || "DARKZONE-MD",
+BOT_NAME: process.env.BOT_NAME || "FATIMA-MD",
 // add bot namw here for menu
-STICKER_NAME: process.env.STICKER_NAME || "DARKZONE-MD",
+STICKER_NAME: process.env.STICKER_NAME || "FATIMA-MD",
 // type sticker pack name 
 CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
 // make this true for custum emoji react    
@@ -73,7 +73,7 @@ AUTO_TYPING: process.env.AUTO_TYPING || "false",
 // true for automatic show typing   
 READ_CMD: process.env.READ_CMD || "false",
 // true if want mark commands as read 
-DEV: process.env.DEV || "923306137477",
+DEV: process.env.DEV || "923155641171",
 //replace with your whatsapp number        
 ANTI_VV: process.env.ANTI_VV || "true",
 // true for anti once view 
