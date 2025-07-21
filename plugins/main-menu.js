9110bd5cd04018fe1a4b1e7043d9b8caec57b610
policeduafatima <policeduafatima@gmail.com>
2025-07-21 16:04:16 +0500

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -346,7 +346,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         await conn.sendMessage(
             from,
             {
-                image: { url: config.MENU_IMAGE_URL || 'https://files.catbox.moe/27sdkb.jpg' },
+                image: { url: config.MENU_IMAGE_URL || 'https://files.catbox.moe/hc962y.jpg' },
                 caption: dec,
                 contextInfo: {
                     mentionedJid: [m.sender],
