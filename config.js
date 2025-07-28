c17c621ebe0fc4020adadaf3b51b235f81f80d92
DUA FATIMA <policeduafatima@gmail.com>
2025-07-28 16:19:12 +0500

    Update config.js

--- config.js
+++ config.js
@@ -43,9 +43,9 @@ DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
 OWNER_NUMBER: process.env.OWNER_NUMBER || "923155641171",
 // add your bot owner number
-OWNER_NAME: process.env.OWNER_NAME || "Dua Fatima",
+OWNER_NAME: process.env.OWNER_NAME || "DUA FATIMA, DUAFATIMA",
 // add bot owner name
-DESCRIPTION: process.env.DESCRIPTION || "*© CREATER Dua Fatima *",
+DESCRIPTION: process.env.DESCRIPTION || "*© CREATER DUA FATIMA * , *DUA FATIMA*",
 // add bot owner name    
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/tdsu0d.jpg",
 // add img for alive msg
@@ -73,10 +73,11 @@ AUTO_TYPING: process.env.AUTO_TYPING || "false",
 // true for automatic show typing   
 READ_CMD: process.env.READ_CMD || "false",
 // true if want mark commands as read 
-DEV: process.env.DEV || "923155641171",
+DEV: process.env.DEV || "923155641171+",
 //replace with your whatsapp number        
 ANTI_VV: process.env.ANTI_VV || "true",
 // true for anti once view 
 AUTO_RECORDING: process.env.AUTO_RECORDING || "false"
 // make it true for auto recoding 
 };
+
