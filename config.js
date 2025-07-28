a7e634ff18c2fe1dc1748891ec111b5399899f61
DUA FATIMA <policeduafatima@gmail.com>
2025-07-28 17:07:49 +0500

    Update config.js

--- config.js
+++ config.js
@@ -27,7 +27,7 @@ ANTI_LINK: process.env.ANTI_LINK || "true",
 // make anti link true,false for groups 
 MENTION_REPLY: process.env.MENTION_REPLY || "false",
 // make true if want auto voice reply if someone menetion you 
-MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/tdsu0d.jpg",
+MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/r2ncqh",
 // add custom menu and mention reply image url
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
@@ -43,11 +43,11 @@ DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
 OWNER_NUMBER: process.env.OWNER_NUMBER || "923155641171",
 // add your bot owner number
-OWNER_NAME: process.env.OWNER_NAME || "DUA FATIMA, DUAFATIMA",
+OWNER_NAME: process.env.OWNER_NAME || "Dua Fatima",
 // add bot owner name
-DESCRIPTION: process.env.DESCRIPTION || "*© CREATER DUA FATIMA * , *DUA FATIMA*",
+DESCRIPTION: process.env.DESCRIPTION || "*© CREATER Dua Fatima *",
 // add bot owner name    
-ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/tdsu0d.jpg",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/r2ncqh",
 // add img for alive msg
 LIVE_MSG: process.env.LIVE_MSG || "> HEY IM ALIVE NOW  *FATIMA-MD*⚡",
 // add alive msg here 
@@ -73,11 +73,10 @@ AUTO_TYPING: process.env.AUTO_TYPING || "false",
 // true for automatic show typing   
 READ_CMD: process.env.READ_CMD || "false",
 // true if want mark commands as read 
-DEV: process.env.DEV || "923155641171+",
+DEV: process.env.DEV || "923155641171",
 //replace with your whatsapp number        
 ANTI_VV: process.env.ANTI_VV || "true",
 // true for anti once view 
 AUTO_RECORDING: process.env.AUTO_RECORDING || "false"
 // make it true for auto recoding 
 };
-
